738fcf1d4d74c6d9654e241ae51e031ce49ab89a
methsanherath18-alt <methsanherath18@gmail.com>
2026-03-03 14:32:37 +0530

    Update server.js

--- be/server.js
+++ be/server.js
@@ -31,7 +31,8 @@ app.use(express.json()); // built-in body parser
 
 const authRoutes = require('./routes/auth.router');
 
-
+const modelRouter = require("./routes/model.router");
+app.use("/api/model", modelRouter);
 
 // Users
 const userRoutes = require("./routes/Users");
